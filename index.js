88e8bac72cf0153cb96942b9eadfe7905f30a882
Alex Steele <alexsteele110@gmail.com>
2018-03-22 13:24:40 -0400

    fixed port listener and screenshot dir

--- index.js
+++ index.js
@@ -26,4 +26,6 @@ app.get('/api/export/:slug', async (req, res) => {
   setTimeout(deleteZip(directories), 5000);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('Listening on port 3000'));
--- screenshotter.js
+++ screenshotter.js
@@ -8,16 +8,12 @@ module.exports = {
     await page.goto(url, {
       waitUntil: 'networkidle2'
     });
-
     
     await page.setViewport({ width: 1200, height: 1200 });
-    await page.screenshot({ path: 'screenshots/desktop-screenshot.png', fullPage: true });
+    await page.screenshot({ path: 'desktop-screenshot.png', fullPage: true });
 
     await page.setViewport({ width: 500, height: 500, isMobile: true });
-    await page.screenshot({ path: 'screenshots/mobile-screenshot.png', fullPage: true });
-
-    // const file1 = `${__dirname}/desktop-screenshot.png`;
-    // const file2 = `${__dirname}/mobile-screenshot.png`;
+    await page.screenshot({ path: 'mobile-screenshot.png', fullPage: true });
 
     await browser.close();
   }
